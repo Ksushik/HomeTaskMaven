9a95a56dcc396b4106c7c23755736e3dd7d43ed4
skillsup <skillsup@skillsup-HP-250-G3-Notebook-PC>
2015-11-24 20:05:16 +0200

    task1

---
+++ src/main/java/web/OrganizerClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import api.MusicSchoolOrganizer;
+import api.Student;
+
+/**
+ *
+ * @author skillsup
+ */
+public class OrganizerClass implements MusicSchoolOrganizer{
+
+    @Override
+    public void addNewStudent(Student newStudent) {
+         
+    }
+    
+}
